b1b888e92e30f301adfea59785b99f3ce3a6cb14
Pavel Leonov <p.leonov@itransition.com>
2018-03-03 11:30:42 +0300

    Added swagger gen

---
+++ init_swagger.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rm -rf ${PWD}/src/app/shared/api/generated
+
+docker pull swaggerapi/swagger-codegen-cli
+docker run --rm -v \
+    ${PWD}/src/app/shared/api:/local swaggerapi/swagger-codegen-cli \
+    generate \
+    -i /local/swagger.json \
+    -l typescript-angular \
+    -o /local/generated
